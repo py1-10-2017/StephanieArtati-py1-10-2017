d5dae823e681e6fb4d85930a80c77b6ce7da5544
faniartati <stephanie_artati@outlook.com>
2017-10-10 13:39:21 -0700

    Counter assignment

--- counter/server.py
+++ counter/server.py
@@ -4,24 +4,20 @@ app.secret_key = 'CounterAppKey'
 
 @app.route('/')
 def index():
-  print("Beginning of root handler")
-  if (session.get('counter') != None):
-    print("Incrementing counter")
-    curr_counter = int(session['counter'])
-    session['counter'] = curr_counter + 1
-  else:
-    print("Initializing counter")
-    session['counter'] = "1"
+  try:
+    session['counter'] += 1
+  except:
+    session['counter'] = 1
   return render_template("counter.html")
 
 @app.route('/doubleincrement')
 def doubleincrement():
-  session['counter'] = int(session['counter']) + 1
+  session['counter'] += 1
   return redirect('/')
 
 @app.route('/reset')
 def reset():
-  session['counter'] = "0"
+  session['counter'] = 0    
   return redirect('/')
 
 app.run(debug=True)
